4c39f3841c52e5afc1506934cf1ce3483321d35e
Amalie Von Behren <vbamalie@gmail.com>
2022-05-06 16:25:38 -0400

    sign up public js done

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,49 @@
 const router = require("express").Router();
 const { Post, Comment, User } = require("../models");
+const withAuth = require("../utils/auth")
 
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
     // get all posts for the homepage
+    try{
+        const dbPostData = await Post.findAll({
+            include:[{model: Comment}]
+        });
+        const postGallery = dbPostData.map((posts)=> posts.get({plain:true})
+        );
+        res.render("posts", {
+            postGallery,
+            loggedIn: req.session.loggedIn
+        });
+    }catch (err) { res.status(500).json(err)}
 });
 
-router.get("/post/:id", async (req, res) => {
+router.get("/post/:id", withAuth, async (req, res) => {
     // get a single post
+    try{
+        const dbPostData = await Post.findByPk(req.params.id, {
+            include[{model: Comment}]
+        });
+        const postGallery = dbPostData.get({plain: true});
+        res.render("single-posts", {postGallery, loggedIn: req.session.loggedIn});
+    } catch (err) {res.status(500).json(err)}
 });
 
 router.get("/login", (req, res) => {
     // login
+    if(req.session.loggedIn){
+        res.redirect("/");
+        return
+    }
+    res.render("login")
 });
 
 router.get("/signup", (req, res) => {
     // signup
+    if(req.session.signUp){
+        res.redirect("/");
+        return
+    }
+    res.render("signup"
 })
 
 module.exports = router;
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,21 +1,24 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const usernameEl = document.querySelector('#username-input-signup');
-    const passwordEl = document.querySelector('#password-input-signup');
-  
+const signupFormHandler = async function (event) {
+  event.preventDefault();
+
+  const usernameEl = document.querySelector('#username-input-signup').value.trim();
+  const passwordEl = document.querySelector('#password-input-signup').value.trim();
+
+  if (usernameEl && passwordEl) {
     const response = await fetch('/api/user', {
-         // Create the functionality to help create the buttons for your website.
-         
+      // Create the functionality to help create the buttons for your website.
+      method: "POST",
+      body: JSON.stringify({ usernameEl, passwordEl }),
+      headers: { "Content-Type": "application/json" },
     });
-  
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
       alert('Failed to sign up');
     }
-  };
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signupFormHandler);
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupFormHandler);
